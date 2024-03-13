4c652d92081d469b143887fadf188e927c70bf2b
Francisco Zuidwijk <electrocisko@gmail.com>
2024-03-13 10:47:10 -0300

    navbar

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const hamburguer = document.getElementById("hamburguer");
-const linksContainer = document.getElementById("links-container")
-
+const linksContainer = document.getElementById("links-container");
+const links = document.getElementsByClassName("links");
 
 hamburguer.addEventListener("click", () => {
-    console.log("clickeado");
-  linksContainer.classList.toggle("hide")
-})
+  linksContainer.classList.toggle("hide");
+});
 
-console.log(linksContainer);
+
+
+for (let i = 0; i < links.length; i++) {
+  const thisLink = links[i];
+  thisLink.addEventListener("click", () => {
+    thisLink.classList.add("active");
+
+    for (let z = 0; z < links.length; z++) {
+      const element = links[z];
+      if (i != z) {
+        element.classList.remove("active");
+      }
+    }
+  });
+}
