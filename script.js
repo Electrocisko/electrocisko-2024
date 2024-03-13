bfc5a6a9661af901719b201541a4e4ab9621258e
Francisco Zuidwijk <electrocisko@gmail.com>
2024-03-13 10:12:57 -0300

    navbar

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo");
+const hamburguer = document.getElementById("hamburguer");
+const linksContainer = document.getElementById("links-container")
+
+
+hamburguer.addEventListener("click", () => {
+    console.log("clickeado");
+  linksContainer.classList.toggle("hide")
+})
+
+console.log(linksContainer);
