cb6846a03993057ec8adcb5bd4a850629fdd0f95
Francisco Zuidwijk <electrocisko@gmail.com>
2024-03-15 09:15:31 -0300

    mapa google

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const hamburguer = document.getElementById("hamburguer");
 const linksContainer = document.getElementById("links-container");
 const links = document.getElementsByClassName("links");
-let displaySize = window.screen.width
+let displaySize = window.screen.width;
+const iconWhatsapp = document.getElementById("icon-whatsapp");
+const contact = document.getElementById("contact");
+const main = document.getElementsByClassName("body-container")[0];
+
+
+
+
+
+
+iconWhatsapp.addEventListener("mouseover", () => {
+  console.log("mouse over");
+contact.classList.remove("hidetext")
+})
+
+
+iconWhatsapp.addEventListener("mouseout", () => {
+  console.log("mouse out");
+contact.classList.add("hidetext")
+})
 
 if (displaySize < 651) {
     linksContainer.classList.add("hide")
@@ -10,6 +29,7 @@ if (displaySize < 651) {
 
 hamburguer.addEventListener("click", () => {
   linksContainer.classList.toggle("hide");
+  main.classList.toggle("padding120")
 });
 
 for (let i = 0; i < links.length; i++) {
