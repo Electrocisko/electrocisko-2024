0d7fd3d5a4a8c0a36e1d0772e496e5c63064208b
Francisco Zuidwijk <electrocisko@gmail.com>
2024-03-18 13:28:32 -0300

    mobil design

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const iconWhatsapp = document.getElementById("icon-whatsapp");
 const contact = document.getElementById("contact");
 const main = document.getElementsByClassName("body-container")[0];
 
-
 iconWhatsapp.addEventListener("mouseover", () => {
   console.log("mouse over");
 contact.classList.remove("hidetext")
@@ -25,13 +24,13 @@ if (displaySize < 651) {
 
 hamburguer.addEventListener("click", () => {
   linksContainer.classList.toggle("hide");
-  //main.classList.toggle("padding120")
 });
 
 for (let i = 0; i < links.length; i++) {
   const thisLink = links[i];
   thisLink.addEventListener("click", () => {
     thisLink.classList.add("active");
+    linksContainer.classList.toggle("hide");
 
     for (let z = 0; z < links.length; z++) {
       const element = links[z];
